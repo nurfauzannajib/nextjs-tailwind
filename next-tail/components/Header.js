f3ac860fe967ba49ced583f25b672e60c5095f9d
Nur Fauzan Najib <nurfauzannajib@gmail.com>
2021-08-14 14:30:18 +0700

    header component

--- next-tail/components/Header.js
+++ next-tail/components/Header.js
@@ -3,9 +3,9 @@ import Logo from "../public/assets/logo.svg";
 
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-4 px-6 md:px-10">
       {/* Left Section */}
-      <div className="relative flex items-center h-10 cursor-pointer">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src={Logo}
           layout="fill"
@@ -13,6 +13,21 @@ function Header() {
           objectPosition="left"
         />
       </div>
+      {/* Middle Section */}
+      <div>
+        <ul className="flex items-center gap-10 justify-center p-3">
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>About</button>
+          </li>
+          <li>
+            <button>Services</button>
+          </li>
+        </ul>
+      </div>
+
       {/* Right Section */}
       <div></div>
     </header>
