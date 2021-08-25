bc250cb2405027a01fca4fdbc8a952d9eebd7eac
Nur Fauzan Najib <nurfauzannajib@gmail.com>
2021-08-25 21:42:20 +0700

    header, again

--- next-tail/components/Header.js
+++ next-tail/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
       </div>
       {/* Middle Section */}
       <div>
-        <ul className="flex items-center gap-10 justify-center p-3">
+        <ul className="items-center gap-14 justify-center p-3 hidden md:flex">
           <li>
             <button>Home</button>
           </li>
@@ -28,8 +28,43 @@ function Header() {
         </ul>
       </div>
 
-      {/* Right Section */}
-      <div></div>
+      {/* Button End */}
+      <div className="flex items-center justify-end">
+        <div className="flex items-center space-x-2 border-2 p-1 rounded-md border-blue-500">
+          <button className="text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </button>
+          <button className="text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
